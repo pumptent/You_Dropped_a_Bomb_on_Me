382a79fa6c40af30b78b57298372e7959f44f3a9
Michael Hank <Michael Hank>
2017-02-05 17:30:33 -0500

    Added script for moving objects from one point to another and added placeholder rooms

---
+++ You_Dropped_a_Bomb_on_Me/Assets/Scripts/CiviliansMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CiviliansMove : MonoBehaviour {
+    private Vector3 pointA;
+    public Vector3 pointB;
+    public float speed;
+    private Vector3 destination;
+	// Use this for initialization
+	void Start () {
+        pointA = transform.position;
+        destination = pointB;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position == pointA)
+        {
+            destination = pointB;
+        }
+
+        if (transform.position == pointB)
+        {
+            destination = pointA;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, destination, Time.deltaTime * speed);
+    }
+}
